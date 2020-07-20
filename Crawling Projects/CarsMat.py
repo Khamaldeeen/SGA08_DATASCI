3bc00d04b61ec5bc688b3fbe3abfac00f9ec5776
Kamaldeen Aiyeleso <Aiyelesok@gmail.com>
2020-07-20 23:25:05 +0100

    Regression model for predicting houserent in Lagos

--- Crawling Projects/CarsMat.py
+++ Crawling Projects/CarsMat.py
@@ -2,38 +2,72 @@ import pandas as pd
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
-import csv
-
-url = 'https://www.cheki.com.ng/vehicles/mercedes-benz?page=4'
-
-source = requests.get(url).text
-soup = BeautifulSoup(source, 'lxml')
-
-mainPage = soup.find('ul', attrs={'class':'listing-unit__container'})
-
-_mainPage = mainPage.find_all('div', attrs = {'class': 'right-card-part-container'})
-
-with open('CarsMat.csv', mode='w', newline='') as outputFile:
-    carPrices = csv.writer(outputFile, delimiter=',', quotechar= '"', quoting = csv.QUOTE_MINIMAL)
-    carPrices.writerow(['Name', 'Model', 'Year', 'Location', 'Transmission', 'Condition', 'Color', 'Amount'])
+import time
 
 
-    for card in _mainPage:
-        car_nameR = card.find('span', attrs = {'class' : 'card-header'})
-        car_name =  car_nameR.find('span', attrs = {'class' : 'ellipses'}).text.strip()
+def CarsMat(url):
+    page = requests.get(url)
+    container = BeautifulSoup(page.content, 'html.parser')
+    cards = container.find_all('div', class_='card-main')
+    pri = container.find_all('div', class_='right-side-desktop-card')
+    #For the transmission, fuel type and condition
+    transmission, condition, engine, lis1 = [], [], [], [] 
+    for item1 in cards:
+        lis = item1.find('ul', attrs = {'class' : 'card-features'}).text.strip()
+        lis1.append(lis)
+    for elem in lis1:
+        t_cards = elem.split()
+        trans = t_cards[1]
+        cond = t_cards[-4] +' ' + 'Used'
+        eng = t_cards[-1]
+        transmission.append(trans)
+        condition.append(cond)
+        engine.append(eng)
+    
+    #The year, make and model of the car
+    Year, Make, Model = [], [], []
+    for item in cards: 
+        car_name =  item.find('span', attrs = {'class' : 'ellipses'}).text.strip()
         car_items = car_name.split()
         year = car_items[0]
         make = car_items[1]
         model = car_items[2]
+        Year.append(year)
+        Make.append(make)
+        Model.append(model)
+    
+    #The price of the cars in Naira 
+    Price = []
+    for items2 in pri:
+        d_pri = items2.find('h2', attrs = {'class' : 'listing-price'}).text.strip()
+        d_pri = d_pri[1:]
+        fd_pri = int(d_pri.replace(',', ''))
+        Price.append(fd_pri)
+    return Year, Make, Model, transmission, condition, engine, Price
 
-        car_price = card.find('h2', attrs = {'class' : 'listing-price'}).text.strip()
-        currency = car_price[:1]
-        price = car_price[1:]
+http = 'https://www.cheki.com.ng/vehicles'
+https = [http]
+for i in range(2,101):
+    child = http + '?page=' + str(i)
+    https.append(child)
+M_Year, M_Make, M_Model, M_trans, M_cond, M_eng, M_Price = [], [], [], [], [], [], []
+for point in https:
+    Year1, Make1, Model1, trans1, cond1, eng1, Price1 = CarsMat(point)
+    M_Year += Year1
+    M_Make += Make1
+    M_Model += Model1
+    M_trans += trans1
+    M_cond += cond1
+    M_eng += eng1
+    M_Price += Price1
 
-        features = card.find('ul', attrs = {'class' : 'card-features'})
-        lists = features.find_all('li', attrs = {'class' : 'ellipses'})
-        color = lists[2].text.strip()
-        location = lists[0].text.strip()
-        transmission = lists[1].text.strip()
-        cond = lists[3].text.strip()
-        carPrices.writerow([make, model, year, location, transmission, cond, color, price])
+
+CarData = pd.DataFrame({'Year': M_Year,
+                        'Make' : M_Make,
+                        'Model' : M_Model,
+                        'Transmission' : M_trans,
+                        'Condition' : M_cond,
+                        'Engine Type' : M_eng,
+                        'Price' : M_Price})
+
+CarData.to_csv('Mat Cars.csv')
